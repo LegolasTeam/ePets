6a935cad2d024ded818f1386ca067456a03bd85f
Bill <baotuan0.0@gmail.com>
2017-09-16 20:35:12 +0700

    Upgrade react native + add maps

--- app/components/newfeeds/feeds.js
+++ app/components/newfeeds/feeds.js
@@ -1,62 +1,54 @@
-import React from 'react';
-import {
-  FlatList,
-  Image,
-  View,
-  TouchableOpacity
-} from 'react-native';
-import {
-  RkText,
-  RkCard, RkStyleSheet
-} from 'react-native-ui-kitten';
-import {SocialBar} from '../socialBar';
-import firebase from '../../utils/firebase';
+import React from "react";
+import { FlatList, Image, View, TouchableOpacity } from "react-native";
+import { RkText, RkCard, RkStyleSheet } from "react-native-ui-kitten";
+import { SocialBar } from "../socialBar";
+import firebase from "../../utils/firebase";
 // import {data} from '../../data';
 // let moment = require('moment');
 
 export default class Feeds extends React.Component {
   static navigationOptions = {
-    title: 'ePets'.toUpperCase()
+    title: "ePets".toUpperCase()
   };
 
   constructor(props) {
     super(props);
 
-  
-    this.state ={
+    this.state = {
       data: []
-    }
+    };
     this.renderItem = this._renderItem.bind(this);
   }
 
   componentDidMount() {
-    firebase.database().ref('Users').on('value', (data) => {
-      var d =[];
-      if (data.val() != null) {
-          
+    firebase
+      .database()
+      .ref("Users")
+      .on("value", data => {
+        var d = [];
+        if (data.val() != null) {
           var a = data.val();
-          for (var key in a){
-            if(a[key].posts!==null)
-              var b = a[key].posts;
-            for(var k in b){
+          for (var key in a) {
+            if (a[key].posts !== null) var b = a[key].posts;
+            for (var k in b) {
               console.log(k);
               let post = {
                 id: k,
                 user: {
-                   name: a[key].username,
-                   ava: a[key].profilepic,
-                   displayName: a[key].displayname
+                  name: a[key].username,
+                  ava: a[key].profilepic,
+                  displayName: a[key].displayname
                 },
                 post: b[k]
-              }
+              };
               d.push(post);
-            }   
+            }
           }
           this.setState({
             data: d
-          })
-      }
-    })
+          });
+        }
+      });
   }
 
   _keyExtractor(post, index) {
@@ -68,31 +60,43 @@ export default class Feeds extends React.Component {
       <TouchableOpacity
         delayPressIn={70}
         activeOpacity={0.8}
-        onPress={() => this.props.navigation.navigate('Feed', {info: info})}>
-        <RkCard rkType='backImg'>
-          <Image rkCardImg source={{uri: info.item.post.url}}/>
+        onPress={() => this.props.navigation.navigate("Feed", { info: info })}
+      >
+        <RkCard rkType="backImg">
+          <Image
+            rkCardImg
+            source={{ uri: info.item.post.url }}
+            style={{ resizeMode: "cover" }}
+          />
           <View rkCardImgOverlay rkCardContent style={styles.overlay}>
-            <RkText rkType='header2 inverseColor'>{info.item.user.displayName}</RkText>
-            <RkText rkType='secondary2 inverseColor'>5 minutes</RkText>
+            <RkText rkType="header2 inverseColor">
+              {info.item.user.displayName}
+            </RkText>
+            <RkText rkType="secondary2 inverseColor">5 minutes</RkText>
             <View rkCardFooter style={styles.footer}>
-              <SocialBar rkType='leftAligned' navigation={this.props.navigation} cmts={info.item.post.comments}/>
-            </View >
+              <SocialBar
+                rkType="leftAligned"
+                navigation={this.props.navigation}
+                cmts={info.item.post.comments}
+              />
+            </View>
           </View>
         </RkCard>
       </TouchableOpacity>
-    )
+    );
   }
 
   render() {
     let info = {};
     info.item = this.state.data[0];
     return (
-      <FlatList data={this.state.data}
-                renderItem={this.renderItem}
-                keyExtractor={this._keyExtractor}
-                style={styles.root}/>
-
-    )
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={this._keyExtractor}
+        style={styles.root}
+      />
+    );
   }
 }
 
@@ -101,7 +105,7 @@ let styles = RkStyleSheet.create(theme => ({
     backgroundColor: theme.colors.screen.base
   },
   overlay: {
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end"
   },
   footer: {
     width: 240
