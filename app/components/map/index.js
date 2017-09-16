dee010cbd5769c75bec7756ca036b2bb11f05f66
Bill <baotuan0.0@gmail.com>
2017-09-16 22:05:05 +0700

    config maps

--- app/components/map/index.js
+++ app/components/map/index.js
@@ -1,26 +1,85 @@
 import React, { Component } from "react";
 
-import { View } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 import MapView from "react-native-maps";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 class Map extends Component {
-  state = {};
+  state = {
+    mapRegion: null,
+    lastLat: null,
+    lastLong: null
+  };
+
+  componentDidMount() {
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      // Create the object to update this.state.mapRegion through the onRegionChange function
+      let region = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.00922 * 1.5,
+        longitudeDelta: 0.00421 * 1.5
+      };
+      this.onRegionChange(region, region.latitude, region.longitude);
+    });
+  }
+
+  onRegionChange(region, lastLat, lastLong) {
+    this.setState({
+      mapRegion: region,
+      // If there are no new values set use the the current ones
+      lastLat: lastLat || this.state.lastLat,
+      lastLong: lastLong || this.state.lastLong
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  onMapPress(e) {
+    // console.log(e.nativeEvent.coordinate.longitude);
+    let region = {
+      latitude: e.nativeEvent.coordinate.latitude,
+      longitude: e.nativeEvent.coordinate.longitude,
+      latitudeDelta: 0.00922 * 1.5,
+      longitudeDelta: 0.00421 * 1.5
+    };
+    this.onRegionChange(region, region.latitude, region.longitude);
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <MapView
-          style={{ top: 0, bottom: 0, left: 0, right: 0, position: "absolute" }}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121
-          }}
-        />
+          style={styles.map}
+          region={this.state.mapRegion}
+          showsUserLocation={true}
+          followUserLocation={true}
+          onRegionChange={this.onRegionChange.bind(this)} //onPress={this.onMapPress.bind(this)}
+          onLongPress={this.onMapPress.bind(this)}
+        >
+          <MapView.Marker
+            title="Dog is here"
+            description="Hahaha"
+            coordinate={{
+              latitude: this.state.lastLat + 0.0005 || -36.82339,
+              longitude: this.state.lastLong + 0.0005 || -73.03569
+            }}
+          >
+            <Icon name="paw" size={30} color="#900" />
+          </MapView.Marker>
+        </MapView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
 
 export default Map;
